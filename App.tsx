3a1393ab6fe10d5045fd2d4785fef1a40cd93d92
Liam Baker <liam.baker2310@gmail.com>
2022-07-19 17:54:55 +0900

    added typescript

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App: FC<{}> = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>
+        EnumeDate
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
