1c814ccb09be1527286d827428c9a73ecb065ad0
Liam Baker <liam.baker2310@gmail.com>
2022-07-19 18:15:24 +0900

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import React, {FC} from 'react';
-import {
-  SafeAreaView,
-  Text,
-} from 'react-native';
+import React, { FC } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './src/screens/home'
+import Settings from './src/screens/settings'
 
+const Stack = createNativeStackNavigator()
 
 const App: FC<{}> = () => {
-
   return (
-    <SafeAreaView>
-      <Text>
-        EnumeDate
-      </Text>
-    </SafeAreaView>
-  );
-};
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
-
-export default App;
+export default App
---
+++ src/screens/settings.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import { SafeAreaView, Text } from 'react-native'
+import {
+  SettingsNavigationProps,
+  SettingsRouteProps,
+} from '../types/navigation'
+
+const Settings: FC<{
+  navigation: SettingsNavigationProps
+  route: SettingsRouteProps
+}> = ({ navigation, route }) => {
+  return (
+    <SafeAreaView>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Settings
