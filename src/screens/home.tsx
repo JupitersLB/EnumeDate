8ed4d50293fb37ede850bf7d3302f0da00e90438
Liam Baker <liam.baker2310@gmail.com>
2022-07-21 23:23:51 +0900

    added basic input with a few fixes

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,20 +1,49 @@
 import React, { FC, useContext } from 'react'
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView, View } from 'react-native'
 import { HomeNavigationProps, HomeRouteProps } from '../types/navigation'
 import { useTailwind } from 'tailwind-rn'
 import RootStoreContext from '../stores/rootStore'
 import JLBButton from '../components/JLBButton'
+import { P } from '../components/texts'
+import JLBInput from '../components/JLBInput'
+import { useForm } from 'react-hook-form'
 
 const Home: FC<{ navigation: HomeNavigationProps; route: HomeRouteProps }> = ({
   navigation,
   route,
 }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ name: string }>()
   const { userStore } = useContext(RootStoreContext)
   const tailwind = useTailwind()
 
+  const onSubmit = (data: { name: string }) => {
+    console.log('form input: ', data.name)
+  }
+
   return (
-    <SafeAreaView>
-      <Text style={tailwind('text-blue-600')}>EnumeDate</Text>
+    <SafeAreaView style={tailwind('flex-1 mx-10 justify-between')}>
+      <P style={tailwind('text-blue-600')}>EnumeDate</P>
+      <View>
+        <JLBInput
+          label="name"
+          control={control}
+          errors={errors}
+          placeholder="enter name"
+        />
+
+        <JLBButton
+          type="solid"
+          color="primary"
+          style={'mt-6'}
+          onPress={handleSubmit(onSubmit)}>
+          Submit
+        </JLBButton>
+      </View>
+
       <JLBButton
         type="solid"
         color="primary"
