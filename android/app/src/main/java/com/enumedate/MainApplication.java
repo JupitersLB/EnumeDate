2f451e232427c046af5c1269c2d18091960fdef9
Liam Baker <liam.baker2310@gmail.com>
2022-08-07 18:57:27 +0900

    remove android version number manual instillation

--- android/app/src/main/java/com/enumedate/MainApplication.java
+++ android/app/src/main/java/com/enumedate/MainApplication.java
@@ -28,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          pacakges.add(new RNVersionNumberPackage())
+          // packages.add(new MyReactNativePackage());)
           return packages;
         }
 
